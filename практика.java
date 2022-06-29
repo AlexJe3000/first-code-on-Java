5d985e21ac9f87ffdfd660cccf4e917e9218145e
Alex <99183741+AlexJe3000@users.noreply.github.com>
2022-06-29 18:26:05 +0300

    Practice.java
    Definition of even and odd numbers

---
+++ практика.java
@@ -0,0 +1,16 @@
+public class практика {
+    public static void main (String[] args) {
+        int a = 100;
+
+        if (a%2==0) {
+            System.out.println("Чётное");
+        }
+        else {
+            System.out.println("Нечётное");
+        }
+
+
+
+
+    }
+}
